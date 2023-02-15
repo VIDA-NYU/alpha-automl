5a4e2bb461a483912b8537086b39ea68a48cc112
Roque Lopez <rlopezc27@gmail.com>
2023-02-15 17:57:18 -0500

    Add example

---
+++ tests/test.py
@@ -0,0 +1,31 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import ShuffleSplit
+import sys
+# TODO: Remove this when the package is implemented, it just a temporal solution
+sys.path.append('/Users/rlopez/D3M/alphad3m-sklearn/alphad3m_sklearn')
+from automl_api import AutoML
+
+diabetes = load_iris()
+X = diabetes.data
+y = diabetes.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=1)
+
+# Add settings
+automl = AutoML('/tmp', time_bound=10, metric='accuracy', split_strategy='holdout')
+#automl = AutoML('/tmp', time_bound=10, metric=accuracy_score, split_strategy=ShuffleSplit())
+
+# Perform the search
+automl.fit(X_train, y_train)
+
+# Plot
+#print(automl.plot_leaderboard())
+
+# Evaluate best model
+y_hat = automl.predict(X_test)
+automl.score(X_test, y_test)
+
+# Evaluate best model externally
+acc = accuracy_score(y_test, y_hat)
+print("Accuracy: %.3f" % acc)
