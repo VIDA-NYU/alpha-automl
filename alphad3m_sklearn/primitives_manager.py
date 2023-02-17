b73461f283468c46d9e4e692704db7b8e78478a1
Roque Lopez <rlopezc27@gmail.com>
2023-02-17 17:12:20 -0500

    Delete file

--- alphad3m_sklearn/primitives_manager.py
+++
@@ -1,9 +0,0 @@
-from sklearn.svm import SVC
-from sklearn.preprocessing import StandardScaler, OneHotEncoder, MinMaxScaler
-
-primitive_list = {
-    'StandardScaler': StandardScaler(),
-    'MinMaxScaler': MinMaxScaler(),
-    'OneHotEncoder': OneHotEncoder(),
-    'SVC': SVC()
-}
