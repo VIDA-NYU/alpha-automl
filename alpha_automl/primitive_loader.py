3fc2ef77061bfc14a776a722a071c81e2bfa7427
Roque Lopez <rlopezc27@gmail.com>
2023-03-07 15:49:14 -0500

    Load primitive types

--- alpha_automl/primitive_loader.py
+++ alpha_automl/primitive_loader.py
@@ -7,25 +7,9 @@ logger = logging.getLogger(__name__)
 PRIMITIVES_HIERARCHY_PATH = join(dirname(__file__), 'resource/primitives_hierarchy.json')
 
 
-def get_primitive_type(primitive_name):
-    primitive_type = None
-    # Changing the primitive families using some predefined rules
-    if primitive_name in {'d3m.primitives.data_cleaning.quantile_transformer.SKlearn',
-                          'd3m.primitives.data_cleaning.normalizer.SKlearn',
-                          'd3m.primitives.normalization.iqr_scaler.DSBOX'}:
-        primitive_type = 'FEATURE_SCALING'
-
-    elif primitive_name in {'d3m.primitives.feature_extraction.feature_agglomeration.SKlearn',
-                            'd3m.primitives.feature_selection.mutual_info_classif.DistilMIRanking'}:
-        primitive_type = 'FEATURE_SELECTION'
-
-
-
-    return primitive_type
-
-
 def create_primitives_hierarchy():
     pass
+
 
 def load_primitives_hierarchy():
     with open(PRIMITIVES_HIERARCHY_PATH) as fin:
@@ -34,6 +18,22 @@ def load_primitives_hierarchy():
 
     return primitives
 
+
+def load_primitives_types():
+    primitive_types = {}
+    with open(PRIMITIVES_HIERARCHY_PATH) as fin:
+        primitives = json.load(fin)
+
+    for primitive_type, primitive_names in primitives.items():
+        for primitive_name in primitive_names:
+            primitive_types[primitive_name] = primitive_type
+
+    logger.info('Primitive types loaded')
+
+    return primitive_types
+
+
+PRIMITIVE_TYPES = load_primitives_types()
 
 if __name__ == '__main__':
     # Run this to create the files for the list and hierarchy of primitives
