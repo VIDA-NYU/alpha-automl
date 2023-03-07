d1668be4683a3079833ffc9517ee797bb318902d
Roque Lopez <rlopezc27@gmail.com>
2023-03-07 15:55:41 -0500

    Use ColumnTransformer for non-numeric features

--- alpha_automl/pipeline_synthesis/pipeline_builder.py
+++ alpha_automl/pipeline_synthesis/pipeline_builder.py
@@ -3,35 +3,29 @@ from copy import deepcopy
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
 from alpha_automl.utils import create_object
+from alpha_automl.primitive_loader import PRIMITIVE_TYPES
 
 logger = logging.getLogger(__name__)
 
 
 def change_default_hyperparams(pipeline_primitives):
-    for pipeline_primitive in pipeline_primitives:
-        if isinstance(pipeline_primitive, OneHotEncoder):
-            pipeline_primitive = OneHotEncoder(handle_unknown='ignore')
-        elif isinstance(pipeline_primitive, SimpleImputer):
-            pipeline_primitive = SimpleImputer(strategy='most_frequent', error_on_no_input=False)
-
-def is_linear_pipeline(pipeline_primitives):
-    return True
+    for i in range(len(pipeline_primitives)):
+        if isinstance(pipeline_primitives[i], OneHotEncoder):
+            pipeline_primitives[i] = OneHotEncoder(handle_unknown='ignore')
+        elif isinstance(pipeline_primitives[i], SimpleImputer):
+            pipeline_primitives[i] = SimpleImputer(strategy='most_frequent', error_on_no_input=False)
 
 
 class BaseBuilder:
 
-    def make_pipeline(self, primitives, automl_hyperparams):
-        pipeline_primitives = self.format_primitves(primitives, automl_hyperparams)
+    def make_pipeline(self, primitives, automl_hyperparams, non_numeric_columns):
+        pipeline_primitives = self.format_primitves(primitives, automl_hyperparams, non_numeric_columns)
         change_default_hyperparams(pipeline_primitives)
-        pipeline = None
-
-        if is_linear_pipeline(pipeline_primitives):
-            pipeline = self.make_linear_pipeline(pipeline_primitives)
-        else:
-            pass  # TODO: Use Column Transformer
-
+        pipeline = self.make_linear_pipeline(pipeline_primitives)
         logger.info(f'New pipelined created:\n{pipeline}')
+
         return pipeline
 
     def make_linear_pipeline(self, pipeline_primitives):
@@ -42,16 +36,34 @@ class BaseBuilder:
     def make_graph_pipeline(self, pipeline_primitives):
         pass
 
-    def format_primitves(self, primitives, automl_hyperparams):
+    def format_primitves(self, primitives, automl_hyperparams, non_numeric_columns):
         pipeline_primitives = []
 
         for primitive in primitives:
             primitive_name = primitive
-            if primitive_name.startswith('sklearn.'):  # It's a regular sklearn class
+            if primitive_name.startswith('sklearn.'):  # It's a regular sklearn primitive
                 primitive_object = create_object(primitive)
             else:
                 primitive_object = deepcopy(automl_hyperparams['new_primitives'][primitive_name]['primitive_object'])
 
+            primitive_type = PRIMITIVE_TYPES[primitive_name]
+            if primitive_type in non_numeric_columns:  # Add a transformer
+                primitive_object = self.add_transformer(primitive_object, primitive_type, non_numeric_columns)
+
             pipeline_primitives.append((primitive_name, primitive_object))
 
         return pipeline_primitives
+
+    def add_transformer(self, primitive_object, primitive_type, non_numeric_columns):
+        column_transformer = None
+
+        if primitive_type == 'TEXT_ENCODER':
+            column_transformer = ColumnTransformer([(f'column_{column}', primitive_object, column)
+                                                    for column in non_numeric_columns[primitive_type]],
+                                                   remainder='passthrough')
+        elif primitive_type == 'CATEGORICAL_ENCODER':
+            column_transformer = ColumnTransformer([('categorical_encoder', primitive_object,
+                                                     non_numeric_columns[primitive_type])],
+                                                   remainder='passthrough')
+
+        return column_transformer
