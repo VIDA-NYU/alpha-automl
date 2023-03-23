474be338d238720775def1fcc65729c771da9f48
Roque Lopez <rlopezc27@gmail.com>
2023-03-23 11:27:23 -0400

    Add test for data_profiler.py

--- tests/test_data_profiler.py
+++ tests/test_data_profiler.py
@@ -1,25 +1,19 @@
-from alpha_automl import AutoMLClassifier
 import pandas as pd
+from os.path import join, dirname
+from alpha_automl.data_profiler import profile_data
 
-if __name__ == '__main__':
-    # Read the datasets
-    output_path = '/Users/rlopez/D3M/tmp/'
-    train_dataset = pd.read_csv('/Users/rlopez/D3M/examples/datasets/299_libras_move/train_data.csv')
-    test_dataset = pd.read_csv('/Users/rlopez/D3M/examples/datasets/299_libras_move/test_data.csv')
 
-    X_train = train_dataset.drop(columns=['class'])
-    y_train = train_dataset[['class']]
-    X_test = test_dataset.drop(columns=['class'])
-    y_test = test_dataset[['class']]
+def test_profile_data():
+    dataset_path = join(dirname(__file__), './test_data/movies.csv')
+    dataset = pd.read_csv(dataset_path)
+    X = dataset.drop(columns=['rating'])
 
-    # Add settings
-    automl = AutoMLClassifier(output_path, time_bound=10)
+    actual_metadata = profile_data(X)
+    expected_metadata = {'nonnumeric_columns': {'CATEGORICAL_ENCODER': [(1, 'type')],
+                                                'TEXT_ENCODER': [(2, 'title'), (3, 'director'), (4, 'cast'),
+                                                                 (5, 'country'), (8, 'duration'), (9, 'listed_in'),
+                                                                 (10, 'description')],
+                                                'DATETIME_ENCODER': [(6, 'date_added')]},
+                         'useless_columns': [], 'missing_values': True}
 
-    # Perform the search
-    automl.fit(X_train, y_train)
-
-    # Plot leaderboard
-    automl.plot_leaderboard(use_print=True)
-
-    # Evaluate best model
-    automl.score(X_test, y_test)
+    assert actual_metadata == expected_metadata
