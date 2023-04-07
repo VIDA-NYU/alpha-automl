7cf9cd4db3e1052bc7c1c752a673e2f7a09372c2
Roque Lopez <rlopezc27@gmail.com>
2023-04-07 11:47:27 -0400

    Load model in 'transform' method

--- examples/my_module.py
+++ examples/my_module.py
@@ -2,17 +2,16 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sentence_transformers import SentenceTransformer
 import numpy as np
 
+embedder = SentenceTransformer('xlm-roberta-base')
+
 
 class MyEmbedder(BaseEstimator, TransformerMixin):
-
-    def __init__(self):
-        self.embedder = SentenceTransformer('xlm-roberta-base')
 
     def fit(self, X, y=None):
         return self
 
     def transform(self, texts):
         text_list = texts.tolist()
-        embeddings = self.embedder.encode(text_list)
+        embeddings = embedder.encode(text_list)
 
         return np.array(embeddings)
