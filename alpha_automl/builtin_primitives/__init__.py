9cdb081dd0f2006555009cb2f668ab30a173cf0b
Yifan Wu <yifanwu@10-18-155-81.dynapool.wireless.nyu.edu>
2023-04-20 14:54:56 -0400

    add three different datetime encoders, and their testcases

---
+++ alpha_automl/builtin_primitives/__init__.py
@@ -0,0 +1,8 @@
+from .datetime_encoder import (CyclicalFeature, Datetime64ExpandEncoder,
+                               DummyEncoder)
+
+__all__ = [
+    "CyclicalFeature",
+    "Datetime64ExpandEncoder",
+    "DummyEncoder",
+]
---
+++ tests/test_datetime_encoder.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+from alpha_automl.builtin_primitives import (CyclicalFeature,
+                                             Datetime64ExpandEncoder,
+                                             DummyEncoder)
+
+
+class TestDatetimeEncoder:
+    """This is the testcases for datetime encoder."""
+
+    df = pd.DataFrame(
+        data={
+            "col1": ["12/1/2009 23:19", "2022-06-30 18:37:00", "2025-09-24 20:37:00"]
+        },
+        index=[0, 1, 2],
+    )
+
+    def test_cyclical_feature(self):
+        encoder = CyclicalFeature()
+        encoder.fit(self.df)
+        np_array = encoder.transform(self.df)
+        assert np_array.shape == (len(self.df), 16)
+
+    def test_datetime64_expand_encoder(self):
+        encoder = Datetime64ExpandEncoder()
+        encoder.fit(self.df)
+        np_array = encoder.transform(self.df)
+        assert (np_array[0] == np.array([2009, 12, 1, 23, 19, 1, 335, 4])).all()
+
+    def test_dummy_encoder(self):
+        encoder = DummyEncoder()
+        encoder.fit(self.df)
+        np_array = encoder.transform(self.df)
+        print(np_array.shape)
+        assert np_array.shape == (len(self.df), 4 * len(self.df))
