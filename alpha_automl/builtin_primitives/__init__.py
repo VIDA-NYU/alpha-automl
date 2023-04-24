75ab4c5678993c301d3faab6b6f9c9c1a7628d77
Eden Wu <yfw215@log-3.hpc.nyu.edu>
2023-04-24 10:33:56 -0400

    resolve review: using absolute path

--- alpha_automl/builtin_primitives/__init__.py
+++ alpha_automl/builtin_primitives/__init__.py
@@ -1,5 +1,5 @@
-from .datetime_encoder import (CyclicalFeature, Datetime64ExpandEncoder,
-                               DummyEncoder)
+from alpha_automl.builtin_primitives.datetime_encoder import (
+    CyclicalFeature, Datetime64ExpandEncoder, DummyEncoder)
 
 __all__ = [
     "CyclicalFeature",
