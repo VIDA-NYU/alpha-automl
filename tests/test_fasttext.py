eb6ce294de536dfc3ab574109a70229de50de4d5
madhuripujari95 <mp5583@nyu.edu>
2023-05-25 13:33:15 -0400

    test fasttext - removed import fasttext

--- tests/test_fasttext.py
+++ tests/test_fasttext.py
@@ -1,8 +1,7 @@
 import numpy as np
 import pandas as pd
 import os
-import fasttext
-
+from alpha_automl._optional_dependency import import_optional_dependency
 from alpha_automl.wrapper_primitives.fasttext import FastTextEmbedderWrapper
 
 
@@ -14,7 +13,7 @@ class TestFastTextEmbedderWrapper:
     
     def test_fasttext_embedder(self):
         os.chdir("..")
-        encoder = FastTextEmbedderWrapper(os.getcwd() + '/examples/cc.en.300.bin')
-        encoder.fit(self.df)
-        np_array = encoder.transform(self.df)
+        embedder = FastTextEmbedderWrapper(os.getcwd() + '/examples/cc.en.300.bin')
+        embedder.fit(self.df)
+        np_array = embedder.transform(self.df)
         assert np_array.shape == (len(self.df), 300)
