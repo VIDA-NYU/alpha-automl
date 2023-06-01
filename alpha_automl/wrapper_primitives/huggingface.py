40d3c74c6118bfb99a1569288036a97e7ef3fe4e
Laiba Mehnaz <laibamehnaz@gmail.com>
2023-06-01 13:29:51 -0400

    cleaned huggingface.py

--- alpha_automl/wrapper_primitives/huggingface.py
+++ alpha_automl/wrapper_primitives/huggingface.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-from sklearn.base import BaseEstimator, TransformerMixin
-import numpy as np
 from transformers import AutoTokenizer, BertTokenizer, RobertaTokenizer, ElectraModel, BertModel, RobertaModel, AutoModel
 import torch
 from sentence_transformers import SentenceTransformer
@@ -11,7 +8,7 @@ class HuggingfaceInterface(BaseEstimator, TransformerMixin):
         '''
         model: Huggingface model class object, for eg: AutoModel(), make sure output_hidden_states=True when instantiating the model class before passing into this class.
         tokenizer: Huggingface tokenizer class object, for eg: AutoTokenizer()
-        ''' 
+        '''
         self.last_four_model_layers = last_four_model_layers
         self.name = name
         self.model = model
@@ -22,72 +19,7 @@ class HuggingfaceInterface(BaseEstimator, TransformerMixin):
 
     def transform(self, texts):
 
-        list_texts = [text for text in texts] #since texts is in the form of numpy array
-        total_length = len(list_texts)
-        
-        if total_length < 32:
-            batch_size = 16
-        else:
-            batch_size = 32
-
-        steps = total_length // batch_size
-        batch_embeddings = []
-
-        for start in range(0, total_length, batch_size):
-            if start  == (steps * batch_size):
-                batch_texts = list_texts[start: total_length]
-
-            else:
-                batch_texts = list_texts[start: start+batch_size]
-
-            ids = self.tokenizer(batch_texts, padding=True, return_tensors="pt")
-            device = 'cuda' if torch.cuda.is_available() else 'cpu'
-            self.model.to(device)
-            ids = ids.to(device)
-            self.model.eval()
-
-            with torch.no_grad():
-                out = self.model(**ids)  #model output contains last_hidden_state, pooler_output, hidden_outputs of each model layer and the embedding layer
-
-            last_hidden_states = out.last_hidden_state
-            sentence_embedding = last_hidden_states[:,0,:]
-
-            sentence_embedding = sentence_embedding.cpu().numpy()
-
-            torch.cuda.empty_cache()
-
-            if start == 0:
-                batch_embeddings = sentence_embedding
-            else:
-                batch_embeddings = np.concatenate((batch_embeddings, sentence_embedding), axis=0)
-
-        return batch_embeddings
-
-=======
-from transformers import AutoTokenizer, BertTokenizer, RobertaTokenizer, ElectraModel, BertModel, RobertaModel, AutoModel
-import torch
-from sentence_transformers import SentenceTransformer
-from sklearn.base import BaseEstimator, TransformerMixin
-import numpy as np
-
-class HuggingfaceInterface(BaseEstimator, TransformerMixin):
-
-    def __init__(self, model, tokenizer, model_name, last_four_model_layers=False):
-        '''
-        model: Huggingface model class object, for eg: AutoModel(), make sure output_hidden_states=True when instantiating the model class before passing into this class.
-        tokenizer: Huggingface tokenizer class object, for eg: AutoTokenizer()
-        '''
-        self.last_four_model_layers = last_four_model_layers
-        self.name = model_name
-        self.model = model
-        self.tokenizer = tokenizer
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, texts):
-
-        list_texts = [text for text in texts] #since texts is in the form of numpy array
+        list_texts = [text for text in texts]  # since texts is in the form of numpy array
         total_length = len(list_texts)
 
         if total_length < 32:
@@ -99,11 +31,11 @@ class HuggingfaceInterface(BaseEstimator, TransformerMixin):
         batch_embeddings = []
 
         for start in range(0, total_length, batch_size):
-            if start  == (steps * batch_size):
+            if start == (steps * batch_size):
                 batch_texts = list_texts[start: total_length]
 
             else:
-                batch_texts = list_texts[start: start+batch_size]
+                batch_texts = list_texts[start: start + batch_size]
 
             ids = self.tokenizer(batch_texts, padding=True, return_tensors="pt")
             device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -112,10 +44,11 @@ class HuggingfaceInterface(BaseEstimator, TransformerMixin):
             self.model.eval()
 
             with torch.no_grad():
-                out = self.model(**ids)  #model output contains last_hidden_state, pooler_output, hidden_outputs of each model layer and the embedding layer
+                out = self.model(
+                    **ids)  # model output contains last_hidden_state, pooler_output, hidden_outputs of each model layer and the embedding layer
 
             last_hidden_states = out.last_hidden_state
-            sentence_embedding = last_hidden_states[:,0,:]
+            sentence_embedding = last_hidden_states[:, 0, :]
 
             sentence_embedding = sentence_embedding.cpu().numpy()
 
@@ -127,4 +60,3 @@ class HuggingfaceInterface(BaseEstimator, TransformerMixin):
                 batch_embeddings = np.concatenate((batch_embeddings, sentence_embedding), axis=0)
 
         return batch_embeddings
->>>>>>> 0cd4f3116210bd8adfa3fad4420263ce1e238418
