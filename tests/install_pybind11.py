be944a7a4cb00d9a752da983c459b1910d9eda99
madhuripujari95 <mp5583@nyu.edu>
2023-06-06 14:41:01 -0400

    subprocess file for pybind installation

--- tests/install_pybind11.py
+++ tests/install_pybind11.py
@@ -3,34 +3,6 @@ import setuptools
 import sys
 import subprocess
 
-class get_pybind_include(object):
-    """Helper class to determine the pybind11 include path
 
-    The purpose of this class is to postpone importing pybind11
-    until it is actually installed, so that the ``get_include()``
-    method can be invoked. """
+subprocess.call([sys.executable, '-m', 'pip', 'install', 'pybind11'])
 
-    def __init__(self, user=False):
-        try:
-            import pybind11
-        except ImportError:
-            if subprocess.call([sys.executable, '-m', 'pip', 'install', 'pybind11']):
-                raise RuntimeError('pybind11 install failed.')
-
-        self.user = user
-
-    def __str__(self):
-        import pybind11
-        return pybind11.get_include(self.user)
-    
-
-if __name__ == '__main__':
-
-        ext_modules=[
-            setuptools.Extension(
-                'your_extension_name',
-                sources=['your_extension_source.cpp'],
-                include_dirs=[get_pybind_include()],
-                language='c++'
-            )
-        ]
