f2d5a63b81d06fa3dfd6ec462e91d85802756d78
Laiba Mehnaz <laibamehnaz@gmail.com>
2023-06-08 03:17:25 -0400

    added test_huggingface.py

---
+++ tests/test_huggingface.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+from sklearn.pipeline import Pipeline
+from alpha_automl.wrapper_primitives.huggingface import HuggingfaceInterface
+
+
+
+class TestHuggingfaceInterface():
+    X = pd.DataFrame(data={'text': ['I`d have responded, if I were going', 'Sooo SAD I will miss you here in San Diego!!!', 'my boss is bullying me..', 'what interview! leave me alone', 'Sons of ****, why couldn`t they put them on the releases we already bought'], 'sentiment': ['neutral', 'negative', 'negative', 'negative', 'negative']}) 
+    y = pd.DataFrame(data={'sentiment': ['neutral', 'negative', 'negative', 'negative', 'negative']})
+
+    test = pd.DataFrame(data={'text': ['I`d have responded, if I were going', 'Sooo SAD I will miss you here in San Diego!!!'], 'sentiment': ['neutral', 'negative'] })
+
+    def test_hf(self):
+
+        pipe = Pipeline(steps=[('alpha_automl.wrapper_primitives.huggingface',HuggingfaceInterface())])
+        pipe.fit(self.X, self.y)
+        x = self.test['text'].values.tolist()
+        pred = pipe.transform(x)
+        assert pred.shape == (2,768)
+        
+        
