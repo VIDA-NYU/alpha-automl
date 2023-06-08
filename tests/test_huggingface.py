b5877bc1f76e676ed7d6fc239392fb7cd0b7db6d
Roque Lopez <rlopezc27@gmail.com>
2023-06-08 16:17:12 -0400

    Add test

--- tests/test_huggingface.py
+++ tests/test_huggingface.py
@@ -1,22 +1,22 @@
-import numpy as np
 import pandas as pd
-from sklearn.pipeline import Pipeline
-from alpha_automl.wrapper_primitives.huggingface import HuggingfaceInterface
+from alpha_automl.wrapper_primitives.huggingface import HuggingfaceEmbedder
 
 
-
-class TestHuggingfaceInterface():
-    X = pd.DataFrame(data={'text': ['I`d have responded, if I were going', 'Sooo SAD I will miss you here in San Diego!!!', 'my boss is bullying me..', 'what interview! leave me alone', 'Sons of ****, why couldn`t they put them on the releases we already bought'], 'sentiment': ['neutral', 'negative', 'negative', 'negative', 'negative']}) 
+class TestHuggingfaceEmbedder():
+    X = pd.DataFrame(data={'text': ['I`d have responded, if I were going',
+                                    'Sooo SAD I will miss you here in San Diego!!!',
+                                    'my boss is bullying me..', 'what interview! leave me alone',
+                                    'Sons of ****, why couldn`t they put them on the releases we already bought']
+                           }
+                     )
     y = pd.DataFrame(data={'sentiment': ['neutral', 'negative', 'negative', 'negative', 'negative']})
 
-    test = pd.DataFrame(data={'text': ['I`d have responded, if I were going', 'Sooo SAD I will miss you here in San Diego!!!'], 'sentiment': ['neutral', 'negative'] })
+    def test_fit_transform(self):
+        from huggingface_hub import snapshot_download
+        model_name = 'google/electra-small-discriminator'
+        snapshot_download(repo_id=model_name)
 
-    def test_hf(self):
-        
-        encoder = HuggingfaceInterface()
+        encoder = HuggingfaceEmbedder(model_name)
         encoder.fit(self.X, self.y)
-        x = self.test['text'].values.tolist()
-        pred = encoder.transform(x)
-        assert pred.shape == (2,768)
-        
-        
+        pred = encoder.transform(self.X['text'].values.tolist())
+        assert pred.shape == (5, 256)
