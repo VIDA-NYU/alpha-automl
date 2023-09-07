7142dd5c7c8f6002df18b5699aac3f6a9d6d08f0
EdenWuyifan <yfw215@nyu.edu>
2023-09-07 15:35:53 -0400

    rmv timeseries from init

--- alpha_automl/builtin_primitives/__init__.py
+++ alpha_automl/builtin_primitives/__init__.py
@@ -3,12 +3,12 @@ from alpha_automl.builtin_primitives.datetime_encoder import (
     Datetime64ExpandEncoder,
     DummyEncoder,
 )
-from alpha_automl.builtin_primitives.time_series_forecasting import (
-    ArimaEstimator,
-    DeeparEstimator,
-    NBEATSEstimator,
-    NHITSEstimator,
-)
+# from alpha_automl.builtin_primitives.time_series_forecasting import (
+#     ArimaEstimator,
+#     DeeparEstimator,
+#     NBEATSEstimator,
+#     NHITSEstimator,
+# )
 from alpha_automl.builtin_primitives.image_encoder import (
     RGB2GrayTransformer,
     HogTransformer,
@@ -21,10 +21,6 @@ __all__ = [
     "CyclicalFeature",
     "Datetime64ExpandEncoder",
     "DummyEncoder",
-    "ArimaEstimator",
-    "DeeparEstimator",
-    "NBEATSEstimator",
-    "NHITSEstimator",
     "RGB2GrayTransformer",
     "HogTransformer",
     "FisherVectorTransformer",
