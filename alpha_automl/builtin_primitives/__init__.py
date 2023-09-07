138e1c1fbed088d907dd4bcbd031bc9ec7bd0334
EdenWuyifan <yfw215@nyu.edu>
2023-09-07 14:58:09 -0400

    add clip

--- alpha_automl/builtin_primitives/__init__.py
+++ alpha_automl/builtin_primitives/__init__.py
@@ -14,6 +14,7 @@ from alpha_automl.builtin_primitives.image_encoder import (
     HogTransformer,
     FisherVectorTransformer,
     SkPatchExtractor,
+    CLIPTransformer,
 )
 
 __all__ = [
@@ -28,4 +29,5 @@ __all__ = [
     "HogTransformer",
     "FisherVectorTransformer",
     "SkPatchExtractor",
+    "CLIPTransformer",
 ]
--- alpha_automl/builtin_primitives/image_encoder.py
+++ alpha_automl/builtin_primitives/image_encoder.py
@@ -1,10 +1,14 @@
 import numpy as np
+import torch
 
 from sklearn.feature_extraction import image
 import skimage
 from skimage.feature import ORB, fisher_vector, hog, learn_gmm
 
 from alpha_automl.base_primitive import BasePrimitive
+from alpha_automl._optional_dependency import import_optional_dependency
+
+clip = import_optional_dependency('clip')
 
 
 class RGB2GrayTransformer(BasePrimitive):
@@ -22,6 +26,32 @@ class RGB2GrayTransformer(BasePrimitive):
     def transform(self, X, y=None):
         """perform the transformation and return an array"""
         return np.array([skimage.color.rgb2gray(img) for img in X])
+
+
+class CLIPTransformer(BasePrimitive):
+    """
+    Convert an array of RGB images to grayscale
+    """
+
+    def __init__(self):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model, _ = clip.load("ViT-B/32", device=self.device)
+        pass
+
+    def fit(self, X, y=None):
+        """returns itself"""
+        return self
+
+    def transform(self, X, y=None):
+        """perform the transformation and return an array"""
+        def clip(img):
+            img = np.transpose(img,(2,0,1))
+            img = torch.from_numpy(img)
+            img = img[None, :, :, :]
+            img = self.model.encode_image(img)
+            img = torch.squeeze(img)
+            return img.detach().cpu().numpy()
+        return np.array([clip(img) for img in X])
 
 
 class HogTransformer(BasePrimitive):
