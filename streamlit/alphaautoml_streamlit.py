54ab9a3a0d283f16eb64718f46c218b5947e9100
Aécio Santos <aecio.solando@gmail.com>
2023-10-24 23:06:27 -0400

    Support upload of ZIP files

--- streamlit/alphaautoml_streamlit.py
+++ streamlit/alphaautoml_streamlit.py
@@ -1,15 +1,20 @@
+import os
 import pickle
 import streamlit as st
 import pandas as pd
 from alpha_automl import AutoMLClassifier
 from sklearn import set_config
 from sklearn.utils import estimator_html_repr
+import tempfile
+import zipfile
+
 
 set_config(display="html")
 
+if "upload_file_id" not in st.session_state:
+    st.session_state.upload_file_id = None
 if "train_dataset" not in st.session_state:
     st.session_state.train_dataset = None
-
 if "automl" not in st.session_state:
     st.session_state.automl = None
 
@@ -23,11 +28,37 @@ st.markdown(
 
 st.divider()
 
-st.header("1. Upload Your CSV File", anchor=False)
-uploaded_file = st.file_uploader("Upload Your CSV File", type=["csv"])
+st.header("1. Upload a CSV or ZIP File", anchor=False)
+st.info(
+    """
+    If you  upload a .zip file, it must contain a CSV file named "data.csv".
+    It may also contain image files that are referenced in a column named "image_path" in the data.csv file.
+    """,
+    icon="ℹ️",
+)
+uploaded_file = st.file_uploader("Upload Your CSV File", type=["csv", "zip"])
 if uploaded_file:
-    if st.session_state.train_dataset is None:
-        train_dataset = pd.read_csv(uploaded_file)
+    print("Uploaded file:", uploaded_file)
+    if st.session_state.upload_file_id != uploaded_file.file_id:
+        print("Opening file:", uploaded_file.name, uploaded_file.type)
+        if uploaded_file.type == "text/csv":
+            train_dataset = pd.read_csv(uploaded_file)
+        elif uploaded_file.type == "application/zip":
+            tmp_dir = tempfile.mkdtemp()
+            with zipfile.ZipFile(uploaded_file) as zip_ref:
+                zip_ref.extractall(tmp_dir)
+            print("Extracted zip file to", tmp_dir)
+            train_dataset = pd.read_csv(os.path.join(tmp_dir, "data.csv"))
+            if "image_path" in train_dataset.columns:
+                train_dataset["image_path"] = train_dataset["image_path"].apply(
+                    lambda x: os.path.join(tmp_dir, x)
+                )
+        else:
+            st.write("Unsupported file type:", uploaded_file.type)
+            st.stop()
+
+        st.session_state.upload_file_id = uploaded_file.file_id
+        st.session_state.train_dataset = train_dataset
     else:
         train_dataset = st.session_state.train_dataset
 
