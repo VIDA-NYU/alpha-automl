261745244aaaf2b2550dfeb349f46997e6f6a5ad
EdenWuyifan <yfw215@nyu.edu>
2024-04-08 14:18:47 -0400

    tmpppp

--- scripts/amlb/user_config/extensions/Alpha-AutoML/exec.py
+++ scripts/amlb/user_config/extensions/Alpha-AutoML/exec.py
@@ -50,7 +50,7 @@ def run(dataset, config):
              f'output_folder: {output_path}\n'
              )
 
-    automl = AutoMLClassifier(time_bound=time_bound, metric=metrics_mapping[metric], time_bound_run=15,
+    automl = AutoMLClassifier(time_bound=10, metric=metrics_mapping[metric], time_bound_run=15,
                               output_folder=output_path, verbose=logging.INFO, save_checkpoint=True)
 
     train_dataset = pd.read_csv(train_dataset_path)
